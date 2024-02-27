0d6170fabf89be2caa2287dab04e3b4eceb25fb4
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-02-27 18:52:14 +0100

    added tooltip to line chart

--- src/entry/pages/Dashboard/components/charts/LineGraph.tsx
+++ src/entry/pages/Dashboard/components/charts/LineGraph.tsx
@@ -1,4 +1,4 @@
-import { LineChart, Line, ResponsiveContainer } from "recharts";
+import { LineChart, Line, ResponsiveContainer, Tooltip } from "recharts";
 
 const LineGraph = ({ data: mainData }: { data: RequestGrowthResponse }) => {
   const data = Array.from({ length: 7 }, (_, i) => ({
@@ -12,6 +12,7 @@ const LineGraph = ({ data: mainData }: { data: RequestGrowthResponse }) => {
   return (
     <ResponsiveContainer width="100%" height="100%">
       <LineChart width={300} height={100} data={data}>
+        <Tooltip />
         <Line type="monotone" dataKey="week1" stroke="#8884d8" strokeWidth={2} />
         <Line type="monotone" dataKey="week2" stroke="#82ca9d" strokeWidth={2} />
       </LineChart>
